4f1975de9f6b8a515271437b8f69c1be5ac92f92
Jainan21 <143272157+Jainan21@users.noreply.github.com>
2025-02-15 00:01:18 +0700

    abcxyz

---
+++ app/children/profile.jsx
@@ -0,0 +1,134 @@
+import { Image, StyleSheet, Text, Touchable, TouchableOpacity, View } from 'react-native'
+import React, { useState } from 'react'
+import { FlashList } from '@shopify/flash-list';
+
+const profile = () => {
+  const Data = [
+    {
+      id: "1",
+      content: 'Personal Details',
+      image: require('@/assets/images/usericon.png')
+    },
+    {
+      id: "2",
+      content: 'Address',
+      image: require('@/assets/images/location.png')
+    },
+    {
+      id: "3",
+      content: 'Payment Method',
+      image: require('@/assets/images/card-payment.png')
+    },
+    {
+      id: "4",
+      content: 'About',
+      image: require('@/assets/images/info.png')
+    },
+    {
+      id: "5",
+      content: 'Help',
+      image: require('@/assets/images/question.png')
+    },
+    {
+      id: "6",
+      content: 'Log out',
+      image: require('@/assets/images/logout.png')
+    },
+  ];
+
+  const [settingData, setSettingData] = useState(Data);
+
+  const renderSizeDrinks = ({ item, index }) => {
+    const { content, image } = item;
+    return (
+      <TouchableOpacity style={styles.buttonItem}>
+        <View style={styles.icon1}>
+          <Image style={styles.icon} source={item.image} />
+        </View>
+        <Text style={styles.textItem}>{item.content}</Text>
+        <Image style={styles.iconNext} source={require("@/assets/images/greater-than-symbol.png")} />
+      </TouchableOpacity>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity style={styles.button}>
+          <Image style={styles.icon} source={require("@/assets/images/back.png")} />
+        </TouchableOpacity>
+        <Text style={styles.headerName}>Setting</Text>
+      </View>
+      <FlashList
+        data={settingData}
+        renderItem={renderSizeDrinks}
+        horizontal={false}
+        estimatedItemSize={200}
+        showsHorizontalScrollIndicator={false}
+
+      />
+    </View>
+  )
+}
+
+export default profile
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#141921',
+    paddingHorizontal: 20,
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 20
+  },
+  button: {
+    width: 30,
+    height: 30,
+    backgroundColor: "#21262E",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 10,
+    padding: 20
+  },
+  icon: {
+    width: 15,
+    height: 15,
+  },
+  headerName: {
+    flex: 1,
+    textAlign: "center",
+    fontSize: 20,
+    fontWeight: 700,
+    color: "#FFFFFF"
+  },
+  buttonItem:{
+    flexDirection:"row",
+    marginVertical:10
+  },
+  icon1:{
+    width:15,
+    height:15,
+    padding:15,
+    borderRadius:20,
+    backgroundColor:'rgba(209, 120, 66, 0.5)',
+    justifyContent:"center",
+    alignItems:"center",
+    marginRight:30
+  },
+  iconNext:{
+    position:"absolute",
+    right:0,
+    width:20,
+    height:20
+  },
+  textItem:{
+    fontSize:16,
+    fontWeight:500,
+    color:"#FFFFFF",
+    alignContent:"center"
+  }
+
+})
--- app/index.jsx
+++ app/index.jsx
@@ -3,8 +3,9 @@ import Register from './register';
 import Login from './login';
 import Home from './home';
 import Detail from './children/detail';
+import Profile from './children/profile';
 
 
 export default function Index() {
-  return <Detail/>;
+  return <Profile/>;
 }
